e125c53e25ade510078158389716efc8a9cc571b
tonyshen-rgb <tonyshen@bu.edu>
2026-03-16 21:53:03 +0000

    Fix production Active Storage config for Render

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,9 +20,9 @@ Rails.application.configure do
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
   # config.require_master_key = true
 
-  # Disable serving static files from the `/public` folder by default since
-  # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
+  # Render
+  config.public_file_server.enabled = ENV["RENDER"].present?
+  config.active_storage.service = :local
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
